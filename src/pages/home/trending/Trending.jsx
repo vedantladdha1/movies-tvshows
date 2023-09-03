d8a7b3b5427302e534816e2b971cd1f72f06a397
vedantladdha1 <laddha.vedant1@gmail.com>
2023-09-03 16:14:56 +0530

    Dummy Commit

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React , {useState} from 'react';
 
-import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
-import SwitchTabs from '../../../components/switchTabs/switchTabs';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper.jsx';
+import SwitchTabs from '../../../components/switchTabs/switchTabs.jsx';
  import useFetch from '../../../hooks/useFetch';
-import Carousel from '../../../components/carousel/Carousel';
+import Carousel from '../../../components/carousel/Carousel.jsx';
 const Trending = () => {
 const [endpoint , setEndpoint] = useState("day");
 const { data , loading } = useFetch(`/trending/movie/${endpoint}`)
